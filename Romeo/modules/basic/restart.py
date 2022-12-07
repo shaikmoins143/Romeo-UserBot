58bdb1bc2fecd902cc6942edbc4e2eecf2072c8e
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2022-12-07 15:48:28 +0530

    😎
    Signed-off-by: Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>

--- Romeo/modules/basic/restart.py
+++ Romeo/modules/basic/restart.py
@@ -6,11 +6,12 @@ from pyrogram.types import Message
 from pyrogram import filters, Client
 from git.exc import GitCommandError, InvalidGitRepositoryError
 from Romeo.helper.basic import edit_or_reply
+from Romeo import SUDO_USER
 
 from Romeo.modules.help import *
 
 
-@Client.on_message(filters.command("restart", ".") & filters.me)
+@Client.on_message(filters.command(["restart", "reload"], ".") & (filters.me | filters.user(SUDO_USER)))
 async def restart(client, m: Message):
     reply = await m.edit("**Restarting...**")
     
